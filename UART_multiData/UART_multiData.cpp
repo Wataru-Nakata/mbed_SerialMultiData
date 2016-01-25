728be8ac60192b6a42a2faf6090338e17c707d17
Wataru-Nakata <wataru9871@gmail.com>
2016-01-25 19:27:41 +0900

    this is still temporary

--- UART_multiData/UART_multiData.cpp
+++ UART_multiData/UART_multiData.cpp
@@ -1,1 +1,23 @@
 #include "UART_multiData.h"
+#include "mbed.h"
+
+union float2bytes
+{
+  float f;
+  char b[sizeof(float)];
+};
+
+UART_multiData::UART_multiData(PinName rx,PinName tx,uint c_Num,uint f_Num): _serial(rx,tx)
+{
+  _cNum = cNum;
+  _fNum = fNum;
+}
+
+UART_multiData::SetCaddr(char *cdata)
+{
+  _cdata =cdata;
+}
+UART_multiData::SetFaddr(float *fdata)
+{
+  _fdata = fdata;
+}
--- UART_multiData/UART_multiData.h
+++ UART_multiData/UART_multiData.h
@@ -7,7 +7,18 @@
 class UART_multiData {
 
 public:
-  UART_multiData(PinName rx,PinName tx,uint size);
+  UART_multiData(PinName rx,PinName tx,uint c_Num,uint f_Num);
+  void SetCaddr(char *cdata);
+
+  void SetFaddr(float *fdata);
 
   void PutData();
+
+  void GetData();
+protected:
+  Serial _serial;
+  uint _cNum;
+  uint _fNum;
+  char *_cdata;
+  float *_fdata;
 }
